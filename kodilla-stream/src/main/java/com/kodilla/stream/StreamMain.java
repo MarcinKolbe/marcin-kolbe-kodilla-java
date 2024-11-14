3cb379a78fe17f7420f5fef61629806329158364
Marcin Kolbe <marcin.kolbe@gmail.com>
2024-11-14 17:44:32 +0100

    7.2 Niemutowalna klasa ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -14,6 +15,9 @@ public class StreamMain {
         poemBeautifier.beautify("   obetnij   ", String::trim);
         poemBeautifier.beautify("NAPISZ MAŁYMI", String::toLowerCase);
         poemBeautifier.beautify("Java", poem -> poem.concat(" 21"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
 //        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 //        System.out.println("Calculating expressions with lambdas");
